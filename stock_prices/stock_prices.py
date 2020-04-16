82d590e3b625ac2509dca7a9038c700aa5d7cea5
yutveg <kodi.0n31l@gmail.com>
2020-04-15 19:43:15 -0700

    stock prices test pass

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -7,21 +7,30 @@ def find_max_profit(prices):
     # sets initial min value
     current_min_price = [prices[0], 0]
     max_profit = 0
+
+    # if only one item no profit
     if(len(prices) <= 1):
         return None
 
+    # iterate through stonks
     for i in range(len(prices)):
 
         print(
             f"current min: {current_min_price[0]}\ncurrent price: {prices[i]}\ncurrent max profit: {max_profit}")
 
+        # if a price offers better profit and is after current min, set it to max profit
         if(prices[i] - current_min_price[0] > max_profit and i > current_min_price[1]):
             print(prices)
             max_profit = prices[i] - current_min_price[0]
 
+        # if a price is less than current min, set it to current min
         if(prices[i] < current_min_price[0]):
             current_min_price[0] = prices[i]
             current_min_price[1] = i
+
+    # if stock tumbles all day, buy and sell last two calls
+    if(max_profit == 0):
+        max_profit = prices[-1] - prices[-2]
 
     return max_profit
 
